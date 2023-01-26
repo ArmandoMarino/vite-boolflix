c3d84d966a08d7c8e31a76db719920f8329aa4fa
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-01-26 16:02:36 +0100

    axios installed and begin app header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: AppHeader
+}
+</script>
+
+<template>
+    <header>
+
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    height: 200px;
+    background-color: black;
+}
+</style>
