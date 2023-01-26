f6f9c29fd6dc6807f0b1d27893106ae4c2a49c49
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-01-26 16:15:31 +0100

    app header and main update markup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script>
+import axios from 'axios';
+import { store } from './data/store';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 export default {
-
+  components: {
+    AppHeader,
+    AppMain
+  },
+  data() {
+    return {
+      store,
+    }
+  }
 }
 </script>
 
 <template>
-
+  <app-header></app-header>
+  <app-main></app-main>
 </template>
 
 
---
+++ src/components/AppMain.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'AppMain'
+}
+</script>
+
+<template>
+    <main>
+
+    </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+    height: 100vh;
+    background-color: #555555;
+}
+</style>
