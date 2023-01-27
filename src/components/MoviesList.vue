ae01273a1f8a8852fe01dd389f77ec5a007531eb
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-01-27 18:08:10 +0100

    tv series card and api done

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -13,19 +13,34 @@ export default {
 </script>
 
 <template>
-    <section id="movies">
-        <div v-if="store.isLoading" class="text-center">Loading...</div>
-        <div v-else class="row m-3 row-cols-2 row-cols-lg-5 g-2 g-lg-3">
-            <movie-card v-for="movie in store.movies" :key="movie.id" :number="movie.number" :name="movie.name"
-                :image="`https://image.tmdb.org/t/p/original/${movie.poster_path}`" :type="movie.type1"></movie-card>
+    <section id="productions">
+        <div id="movies">
+            <h1>Movies</h1>
+            <div v-if="store.isLoading" class="text-center">Loading...</div>
+            <div v-else class="row m-3 row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+                <movie-card v-for="movie in store.movies" :key="movie.id" :number="movie.number" :name="movie.name"
+                    :image="`https://image.tmdb.org/t/p/original/${movie.poster_path}`"
+                    :type="movie.type1"></movie-card>
+            </div>
         </div>
+
+
+        <div id="series">
+            <h1>TV Series</h1>
+            <div v-if="store.isLoading" class="text-center">Loading...</div>
+            <div v-else class="row m-3 row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+                <movie-card v-for="serie in store.series" :key="serie.id" :number="serie.number" :name="serie.name"
+                    :image="`https://image.tmdb.org/t/p/original/${serie.poster_path}`"></movie-card>
+            </div>
+        </div>
+
     </section>
 
 
 </template>
 
 <style lang="scss" scoped>
-#movies {
+#productions {
     border-radius: 20px;
 }
 </style>
